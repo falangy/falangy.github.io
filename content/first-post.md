a180a3e7dd6f4f01b8cc8e6f0d95d9a68541ccdd
falangy <falangy@gmail.com>
2020-04-23 14:27:39 +0300

    hi

---
+++ content/first-post.md
@@ -0,0 +1,5 @@
+Title: My First Review
+Date: 2020-04-23 10:20
+Category: 3D
+
+Following is a review of my favorite mechanical keyboard.
---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Ariel Shaked'
+SITENAME = '3D ART AND STUFF'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Jerusalem'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('ArtStation', 'https://arielshak.artstation.com/'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
